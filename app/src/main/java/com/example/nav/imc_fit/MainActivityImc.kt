15835e63a676bdc747eb41141b45446bc6dd195a
Pedro Rogel <pedrorogel3@gmail.com>
2025-03-25 14:58:22 -0300

    feat: creating main imc

---
+++ app/src/main/java/com/example/nav/imc_fit/MainActivityImc.kt
@@ -0,0 +1,21 @@
+package com.example.nav.imc_fit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.nav.R
+
+class MainActivityImc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_imc)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
